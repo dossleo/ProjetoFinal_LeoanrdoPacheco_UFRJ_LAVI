39c1fcedb2e4b09d7cd3fda656d9404bc20171a8
dossleo <leonardo.pacheco.santos@outlook.com>
2022-10-23 10:08:37 -0300

    novo código de frequência

--- code/frequency_features_extraction.py
+++ code/frequency_features_extraction.py
@@ -6,42 +6,93 @@ import pandas as pd
 import os
 import glob
 from math import sqrt
-from .general_decorators import logger
+import scipy.fftpack
 
 class Frequency_Features_Extraction():
-    def __init__(self,path,rolamento):
+    def __init__(self,path):
+        self.path = path
         self.filename = '2004.02.12.10.32.39'
         self.dataset=pd.read_csv(os.path.join(path, self.filename), sep='\t',header=None)
 
         self.bearing_no = 1
         self.bearing_data = np.array(self.dataset.iloc[:,self.bearing_no-1])
 
-        #self.feature_matrix=np.zeros((1,9))
+        self.feature_matrix=np.zeros((1,9))
 
         self.length = len(self.bearing_data)
-    
-    @logger
-    def FFT(self):
-        self.fourier = np.fft.fftfreq(self.bearing_data)
-        return self.fourier
 
-    @logger
+        self.freq_sample = 20480
+        self.rpm = 2000
+
+        self.time_vector = np.linspace(0,1,self.freq_sample)
+
+        self.raw_data = np.array((self.bearing_data,self.time_vector))
+
+        # Dados encontrados em https://www.rexnord.com/products/za2115
+        self.Frequency_Fundamental_Train = 0.0072
+        self.Frequency_Inner_Ring_Defect = 0.1617
+        self.Frequency_Outer_Ring_Defect = 0.1217
+        self.Frequency_Roller_Spin = 0.0559
+
+        
+        # Aplicando a transformada de Fourier
+        self.fourier = scipy.fftpack.fft(self.raw_data)
+
+        self.xf = self.time_vector
+        self.yf = self.fourier
+        self.N = self.length
+
+        self.yf = self.yf[0,:]
+
+        # Verificar se esta discretização da frequência está correta
+        self.xf = np.linspace(0,self.freq_sample//2,self.freq_sample//2)
+
+        # Janela de pico
+        self.window = 10
+
+
+        # Retirado do código em matlab
+
+        self.k = np.linspace(0,self.N-1,self.N)                          # k é um vetor que vai de zero até N menos 1
+        self.T = self.N/self.freq_sample                           # Vetor de tempo N dividido pela frequência de amostragem
+    
+        self.X = np.fft.fftn(self.raw_data)/self.N                      # X recebe a FFT normalizada do vetor x sobre N
+        self.cutOff = (self.N//2)                 # cutOff ajusta o eixo X
+        self.X = self.X[1:self.cutOff]
+        self.freq = np.linspace(1,self.cutOff,self.cutOff+1)
+        # figure()
+        # plot(freq(1:cutOff),abs(X))        # Plota a transformada de Fourier e o valor de X em módulo
+
+    
+
+    def plot_fourier(self):
+        self.yf = self.rms()
+        self.fig, self.ax = plt.subplots()
+        self.ax.plot(self.xf, np.abs(self.yf[:self.N//2]))
+        plt.title('Legenda')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Amplitude')
+        plt.show()
+
     def PicosRPM(self):
         pass
     
-    @logger
     def PicosPistaExterna(self):
         pass
 
-    @logger
     def PicosPistaInterna(self):
         pass
 
-    @logger
     def PicosGaiola(self):
         pass
 
-    @logger
     def PicosRolo(self):
         pass
 
+    def rms(self):
+        self.rms_value = sqrt(sum(n*n for n in self.bearing_data[0:len(self.bearing_data)*0.01])/self.length)        
+        return self.rms_value 
+
+Teste = Frequency_Features_Extraction('C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/brutos/2nd_test')
+
+Teste.plot_fourier()
--- code/time_features_extraction.py
+++ code/time_features_extraction.py
@@ -20,43 +20,31 @@ class TimeFeatureExtraction():
         self.bearing_no = 1
         self.bearing_data = np.array(self.dataset.iloc[:,self.bearing_no-1])
 
-        #self.feature_matrix=np.zeros((1,9))
-
         self.length = len(self.bearing_data)
 
     @logger
     def maximum(self):
-        self.max = np.max(self.bearing_data)
-        
-        #self.feature_matrix[0,0] = self.max
+        self.max = np.max(self.bearing_data)        
         return self.max
 
     @logger
     def minimum(self):
-        self.min = np.min(self.bearing_data)
-        
-        #self.feature_matrix[0,1] = self.min
+        self.min = np.min(self.bearing_data)        
         return self.min
 
     @logger
     def mean(self):
-        self.media = np.mean(self.bearing_data)
-        
-        #self.feature_matrix[0,2] = self.media
+        self.media = np.mean(self.bearing_data)        
         return self.media
 
     @logger
     def standard_deviation(self):
-        self.std = np.std(self.bearing_data, ddof = 1)
-        
-        #self.feature_matrix[0,3] = self.std
+        self.std = np.std(self.bearing_data, ddof = 1)        
         return self.std
 
     @logger
     def rms(self):
-        self.rms_value = sqrt(sum(n*n for n in self.bearing_data)/self.length)
-        
-        #self.feature_matrix[0,4] = self.rms_value
+        self.rms_value = sqrt(sum(n*n for n in self.bearing_data)/self.length)        
         return self.rms_value 
 
     @logger
@@ -66,7 +54,6 @@ class TimeFeatureExtraction():
         self.s_3 = np.std(self.bearing_data, ddof = 1) ** 3
         self.skew = self.third_moment/self.s_3
 
-        #self.feature_matrix[0,5] = self.skew
         return self.skew
 
     @logger
@@ -76,22 +63,47 @@ class TimeFeatureExtraction():
         self.s_4 = np.std(self.bearing_data, ddof = 1) ** 4
         self.kurt = self.fourth_moment / self.s_4 - 3
 
-        #self.feature_matrix[0,6] = self.kurt
         return self.kurt
 
     @logger
     def crest_factor(self):
         self.cf = self.max/self.rms_value
 
-        #self.feature_matrix[0,7] = self.cf
         return self.cf
 
     @logger
     def form_factor(self):
         self.ff = self.rms_value/self.media
 
-        #self.feature_matrix[0,8] = self.ff
         return self.ff
+
+    @logger
+    def data_visualization(self):
+        df1 = pd.read_csv("C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/Tratados/Time_feature_matrix_Bearing_1_Test_2.csv")
+        df1 = pd.read_csv("C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/Tratados/Time_feature_matrix_Bearing_1_Test_2.csv",index_col='Unnamed: 0')
+        df1.index = pd.to_datetime(df1.index)
+        
+        df1 = pd.read_csv("C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/Tratados/Time_feature_matrix_Bearing_1_Test_2.csv",index_col='Unnamed: 0')
+        df2 = pd.read_csv("C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/Tratados/Time_feature_matrix_Bearing_2_Test_2.csv",index_col='Unnamed: 0')
+        df3 = pd.read_csv("C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/Tratados/Time_feature_matrix_Bearing_3_Test_2.csv",index_col='Unnamed: 0')
+        df4 = pd.read_csv("C:/Users/leona/Documents/ProjetoFinal_LeonardoPacheco_UFRJ_LAVI/database/Tratados/Time_feature_matrix_Bearing_4_Test_2.csv",index_col='Unnamed: 0')
+
+        df1.index = pd.to_datetime(df1.index)
+
+        for col in (df1.columns):  
+    
+            plt.figure(figsize=(10, 5))
+            plt.plot(df1.index,df1[col])
+            plt.plot(df1.index,df2[col])
+            plt.plot(df1.index,df3[col])
+            plt.plot(df1.index,df4[col])
+
+            plt.legend(['bearing-1','bearing-2','bearing-3','bearing-4'])
+
+            plt.xlabel("Date-Time")
+            plt.ylabel(col)
+            plt.title(col)
+            plt.show()
         
     @logger
     def execute_time_features(self):
@@ -99,30 +111,37 @@ class TimeFeatureExtraction():
         self.Time_feature_matrix=pd.DataFrame()
 
         self.test_set=2
+        for bearing_no in range(4):
+            self.bearing_no= bearing_no # Provide the Bearing number [1,2,3,4] of the Test set '   
 
-        self.bearing_no=1 # Provide the Bearing number [1,2,3,4] of the Test set
+            for filename in os.listdir(path):
+                
+                self.dataset=pd.read_csv(os.path.join(path, filename), sep='\t',header=None)
 
-        for filename in os.listdir(path):
-            
-            self.dataset=pd.read_csv(os.path.join(path, filename), sep='\t',header=None)
+                self.bearing_data = np.array(self.dataset.iloc[:,self.bearing_no])
 
-            self.bearing_data = np.array(self.dataset.iloc[:,self.bearing_no-1])
+                self.feature_matrix=np.zeros((1,9))
 
-            self.feature_matrix=np.zeros((1,9))
+                self.feature_matrix[0,0] = self.maximum()
+                self.feature_matrix[0,1] = self.minimum()
+                self.feature_matrix[0,2] = self.mean()
+                self.feature_matrix[0,3] = self.standard_deviation()
+                self.feature_matrix[0,4] = self.rms()
+                self.feature_matrix[0,5] = self.skewness()
+                self.feature_matrix[0,6] = self.kurtosis()
+                self.feature_matrix[0,7] = self.crest_factor()
+                self.feature_matrix[0,8] = self.form_factor()
+                
+                self.df = pd.DataFrame(self.feature_matrix)
+                self.df.index=[filename[:-3]]
+                
+                self.frames = [self.Time_feature_matrix,self.df]
+                self.Time_feature_matrix = pd.concat(self.frames)
 
-            self.feature_matrix[0,0] = self.maximum()
-            self.feature_matrix[0,1] = self.minimum()
-            self.feature_matrix[0,2] = self.mean()
-            self.feature_matrix[0,3] = self.standard_deviation()
-            self.feature_matrix[0,4] = self.rms()
-            self.feature_matrix[0,5] = self.skewness()
-            self.feature_matrix[0,6] = self.kurtosis()
-            self.feature_matrix[0,7] = self.crest_factor()
-            self.feature_matrix[0,8] = self.form_factor()
-            
-            self.df = pd.DataFrame(self.feature_matrix)
-            self.df.index=[filename[:-3]]
-            
-            self.frames = [self.Time_feature_matrix,self.df]
-            self.Time_feature_matrix = pd.concat(self.frames)
 
+            self.Time_feature_matrix.columns = ['Max','Min','Mean','Std','RMS','Skewness','Kurtosis','Crest Factor','Form Factor']
+            self.Time_feature_matrix.index = pd.to_datetime(self.Time_feature_matrix.index, format='%Y.%m.%d.%H.%M')
+
+            self.Time_feature_matrix = self.Time_feature_matrix.sort_index()
+
+            self.Time_feature_matrix.to_csv('Time_feature_matrix_Bearing_{}_Test_{}.csv'.format(self.bearing_no,self.test_set))
