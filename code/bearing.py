53debbe463522794c2256bc2af0b7f567f1684ce
dossleo <leonardo.pacheco.santos@outlook.com>
2022-10-23 10:43:43 -0300

    frequencia de defeito em função da RPM

--- code/bearing.py
+++ code/bearing.py
@@ -2,25 +2,35 @@ from math import *
 
 class Bearing():
 
-    def __init__(self,rpm,d_pista_externa,d_pista_interna,n_rolos,phi = 0):
+    def __init__(self,d_pista_externa,d_pista_interna,n_rolos,phi = 0):
+        '''
+        Unidades de medida linear em milímetro
+        Unidades de medida angular em rad
 
-        self.d_rolo = (self.d_pista_externa - self.d_pista_interna)
-        self.d_medio = (self.d_pista_externa - self.d_pista_interna)/2
-        self.freq_rot = self.rpm*2*pi/60
+        Output precisa ser multiplicado pela frequência de rotação em Hz
+        '''
+        
+        self.d_pista_externa = d_pista_externa
+        self.d_pista_interna = d_pista_interna
+        self.n_rolos = n_rolos
+        self.phi = phi
+
+        self.d_rolo = (self.d_pista_externa - self.d_pista_interna)/2
+        self.d_medio = (self.d_pista_externa + self.d_pista_interna)/2
 
     def freq_outer_race(self):
-        #self.freq_rot = self.rpm*2*pi/60
-        return self.freq_rot*self.n_rolos*0.5*(1-(self.d_rolo/self.d_medio)*cos(self.phi))
+        self.BPFO = (self.n_rolos/2)*(1-(self.d_rolo/self.d_medio)*cos(self.phi))
+        return self.BPFO
 
     def freq_innter_race(self):
-        #self.freq_rot = self.rpm*2*pi/60
-        return self.freq_rot*self.n_rolos*0.5*(1+(self.d_rolo/self.d_medio)*cos(self.phi))
+        self.BPFI = (self.n_rolos/2)*(1+(self.d_rolo/self.d_medio)*cos(self.phi))
+        return self.BPFI
     
     def freq_gaiola(self):
-        #self.freq_rot = self.rpm*2*pi/60
-        return self.freq_rot*0.5*(1-(self.d_rolo/self.d_medio)*cos(self.phi))
+        self.FTF = 0.5*(1-(self.d_rolo/self.d_medio)*cos(self.phi))
+        return self.FTF
     
     def freq_rolo(self):
-        #self.freq_rot = self.rpm*2*pi/60
-        return (self.d_medio/(2*self.d_rolo))*(1-((self.d_rolo/self.d_medio)*cos(self.phi))**2)
+        self.BSF = (self.d_medio/self.d_rolo)*(1-((self.d_rolo/self.d_medio)*cos(self.phi))**2)
+        return self.BSF
 
