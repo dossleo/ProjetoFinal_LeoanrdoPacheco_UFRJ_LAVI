e40730e661218516bd48de59fb124c154c58d8cb
dossleo <leonardo.pacheco.santos@outlook.com>
2022-11-27 16:06:08 -0300

    problem with frequencia_referencia

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    dataframe = []
     # Definição de frequência de aquisição
     fs = models.freq_sample
     
@@ -17,9 +16,10 @@ if __name__ == "__main__":
     rpm_medio = pegar_rpm.get_rpm_medio()
     # pegar_rpm.plot_rpm()
 
-
+for fault in range(len(models.fault_frequency)):
 # Passo 1: Descobrir maior frequência de defeito do rolamento
     maior_freq_defeito = max(models.fault_frequency)
+    dataframe = []
     for file in models.filenames:
 
     # Passo 2: Passar filtro passa baixa um pouco acima dessa frequência
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         order = 5
 
         # Definindo frequência de aplicação do filtro
-        cutoff = rpm_medio*2
+        cutoff = models.rpm*2
 
         dados_filtrados = _low_pass_filter.LowPassFilter(raw_data,cutoff,order)
         # dados_filtrados.PlotTimeDomain(plot_raw_data = False)
@@ -49,16 +49,16 @@ if __name__ == "__main__":
 
     # Passo 5: Aplicar FFT
 
-        dominio_frequencia = _frequency_features_extraction.FrequencyFeaturesExtraction(dados_normalizados.Get(),rpm_medio)
-        frequencia_referencia = models.frequency_outer_ring_defect
+        dominio_frequencia = _frequency_features_extraction.FrequencyFeaturesExtraction(dados_normalizados.Get(),models.rpm)
+        frequencia_referencia = models.fault_frequency[fault]*15
         ordens = 5
-        janela = 60
-        # dominio_frequencia.PlotFrequencyDomain(frequencia_referencia,ordens)
-        # dominio_frequencia.PlotJanela(frequencia_referencia,janela)
+        janela = 40
+        dominio_frequencia.PlotFrequencyDomain(frequencia_referencia,ordens)
+        dominio_frequencia.PlotJanela(frequencia_referencia,janela)
 
     # Passo 6: Aplicar métricas do domínio do tempo nas janelas de frequência
         media = dominio_frequencia.MediaOrdens(frequencia_referencia,janela,ordens)
-        metricas = dominio_frequencia.metricas
+        # metricas = dominio_frequencia.metricas
 
         dataframe.append(media)
 
@@ -71,9 +71,9 @@ if __name__ == "__main__":
 
 
     # Get RPM
-dataframe = pd.json_normalize(dataframe)
-for feature in models.features:
-    plt.plot(range(len(models.filenames)),dataframe[feature])
-    plt.title(feature)
-    plt.show()
-breakpoint()
+    dataframe = pd.json_normalize(dataframe)
+    for feature in models.features:
+        plt.plot(range(len(models.filenames)),dataframe[feature])
+        plt.title(feature + "-" + str(models.fault_frequency[fault]))
+        plt.show()
+# breakpoint()
