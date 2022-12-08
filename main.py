7fc5a7ba23485b939e785ba8cd7e50ac9005252b
dossleo <leonardo.pacheco.santos@outlook.com>
2022-12-08 18:53:47 -0300

    generate all csv files

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from models import (run)
 
 for test_number in range(len(models.PATH_TEST)):
     for bearing in models.bearings[test_number]:
-        teste = f'test{test_number+1}'
+        nome_teste = f'test{test_number+1}'
         rolamento = bearing
         path =  models.PATH_TEST[test_number]
         column = models.bearings[test_number][bearing]
         filter_order = 5
-        order_frequency=6
+        order_frequency=8
         window_frequency=40
 
         modelo = run.GenerateCSV(path =path,
@@ -17,4 +17,4 @@ for test_number in range(len(models.PATH_TEST)):
                                 order_frequency=order_frequency,
                                 window_frequency = window_frequency)
 
-        modelo.save_as_csv(name = f'features_{teste}_{rolamento}_filter{filter_order}_order{order_frequency}_window{window_frequency}')
+        modelo.save_as_csv(name = f'features_{nome_teste}_{rolamento}_filter{filter_order}_order{order_frequency}_window{window_frequency}')
