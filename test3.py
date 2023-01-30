14144172c184a5cafd44f3f820a602f5e255e448
dossleo <leonardo.pacheco.santos@outlook.com>
2023-01-30 11:31:56 -0300

    teste para leitura de rpm

--- test3.py
+++ test3.py
@@ -1,17 +1,21 @@
 import models
 import os
 import pandas as pd
-from models import get_raw_data
+from models import get_raw_data, get_rpm
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-pasta = f'database/{models.testes[0]}/{models.defeitos_desalinhamento_horizontal[0]}'
+pasta = 'database/normal'
 
 arquivos = os.listdir(pasta)
 
+tamanho = 150000
+
 sinal = get_raw_data.GetData(pasta,arquivos[0],0)
 sinal = sinal.Get()
+# sinal = pd.json_normalize(sinal)
+
 
 frequencia_aquisicao = models.freq_aquisicao
 n_points = len(sinal)
@@ -21,4 +25,16 @@ t_total = n_points/frequencia_aquisicao
 vetor_tempo = np.linspace(0,t_total,n_points)
 
 plt.plot(vetor_tempo,sinal)
+# plt.axis('off')
 plt.show()
+
+rpm = get_rpm.GetRPM(sinal,frequencia_aquisicao)
+rpm_medio = rpm.get_rpm_medio()
+print(rpm_medio)
+rpm.plot_picos()
+rpm.plot_rpm()
+
+print(rpm)
+
+
+breakpoint()
